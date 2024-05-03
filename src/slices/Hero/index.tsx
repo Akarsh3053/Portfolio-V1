80df45795d16c5f6cad70ff0fc031ab36dbb0eae
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-05-03 21:21:48 +0530

    add layout

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,6 +4,7 @@ import gsap from 'gsap';
 import { useEffect, useRef } from 'react';
 import { SliceComponentProps } from "@prismicio/react";
 import { Content, KeyTextField } from "@prismicio/client";
+import Bounded from '@/components/Bounded';
 
 /**
  * Props for `Hero`.
@@ -71,7 +72,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
   }
 
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
@@ -87,7 +88,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           </span>
         </div>
       </div>
-    </section >
+    </Bounded>
   );
 };
 
