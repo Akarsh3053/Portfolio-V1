fa71702d41c590ac98693c1b986e5742ff3e7410
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-05-04 23:30:11 +0530

    add sounds and effects to shapes

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -79,7 +79,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
-        {/* <Shapes /> */}
+        <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
             <span className="block text-slate-300">{renderLetters(slice.primary.first_name, "first")}</span>
