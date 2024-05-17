c04b6b467e04a2cedc3d7c22141c16e880b812ce
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-05-17 18:47:44 +0530

    add content list from cms

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,6 +1,34 @@
-const ContentList = () => {
+import { Content } from "@prismicio/client"
+
+type ContentListProps = {
+    items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+    contentType: Content.ContentIndexSlice["primary"]["content_type"];
+    fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+    viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+};
+
+
+
+const ContentList = ({ items, contentType, fallbackItemImage, viewMoreText = "Read More" }: ContentListProps) => {
     return (
-        <div>ContentList</div>
+        <div>
+            <ul>
+                {items.map((item, index) => (
+                    <li>
+                        <a href="">
+                            <div>
+                                <span>{item.data.title}</span>
+                                <div>
+                                    {item.tags.map(() => ())}
+                                </div>
+                                <span>{viewMoreText}</span>
+                            </div>
+
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
