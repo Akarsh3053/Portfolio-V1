2a56b33f7421b5d1e555d5ddb04098e5c3a32fd5
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-05-21 23:06:27 +0530

    add noise texture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
 import "./globals.css";
+import clsx from "clsx";
+import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Urbanist } from "next/font/google";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -18,10 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-slate-900 text-slate-100">
-      <body className={urbanist.className}>
+      <body className={clsx(urbanist.className, "relative min-h-screen")}>
         <Header />
         {children}
         <Footer />
+        <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
       </body>
     </html>
   );
