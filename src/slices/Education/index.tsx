e26b393068fe4ce7d34f486a8ab7dce00bad5f31
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-06-11 22:21:32 +0530

    configure education slice

--- src/slices/Education/index.tsx
+++ src/slices/Education/index.tsx
@@ -1,23 +1,43 @@
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
- * Props for `Education`.
+ * Props for `Experience`.
  */
-export type EducationProps = SliceComponentProps<Content.EducationSlice>;
+export type ExperienceProps = SliceComponentProps<Content.EducationSlice>;
 
 /**
- * Component for "Education" Slices.
+ * Component for "Experience" Slices.
  */
-const Education = ({ slice }: EducationProps): JSX.Element => {
+const Experience = ({ slice }: ExperienceProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for education (variation: {slice.variation}) Slices
-    </section>
+      <Heading as="h2" size="lg">
+        {slice.primary.heading}
+      </Heading>
+      {slice.items.map((item, index) => (
+        <div key={index} className="ml-6 mt-8 max-w-prose md:ml-12 md:mt-16">
+          <Heading as="h3" size="sm">
+            {item.title}
+          </Heading>
+
+          <div className="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-tight text-slate-400">
+            <span>{item.timeline}</span>{" "}
+            <span className="text-3xl font-extralight">/</span>{" "}
+            <span>{item.institution}</span>
+          </div>
+          <div className="prose prose-lg prose-invert mt-4">
+            <PrismicRichText field={item.description} />
+          </div>
+        </div>
+      ))}
+    </Bounded>
   );
 };
 
-export default Education;
+export default Experience;
