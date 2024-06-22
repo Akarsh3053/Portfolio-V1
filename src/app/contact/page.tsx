50a4efccd2bf2ee545221f8d01e34692ceb70890
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-06-22 23:19:42 +0530

    add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Bounded from "@/components/Bounded";
+
+export default function Contact() {
+
+    return (
+        <Bounded>
+            <div>Hello World</div>
+        </Bounded>
+    );
+}
