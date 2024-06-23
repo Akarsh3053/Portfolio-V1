11c1c4d2e11a6b051eb3956e4a3ea5eba7f352c2
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-06-23 10:28:01 +0530

    intefrating visi

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,8 +3,9 @@ import Bounded from "@/components/Bounded";
 export default function Contact() {
 
     return (
-        <Bounded>
-            <div>Hello World</div>
-        </Bounded>
+        <>
+            <div className="visme_d" data-title="Clean Contact Form" data-url="x401ry90-clean-contact-form" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="77681">
+            </div><script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script>
+        </>
     );
 }
