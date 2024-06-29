ad45986234f00ad6d076a10d65d837ba7a835991
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-06-29 11:21:23 +0530

    final config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,20 @@ import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Urbanist } from "next/font/google";
+import { PrismicPreview } from "@prismicio/next";
+import { createClient, repositoryName } from "@/prismicio";
+import { title } from "process";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Akarsh Bajpai",
-  description: "Hi! I am Akarsh, a fullstack developer this is my portfolio where you can learn about me and see my latest work.",
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  return {
+    title: settings.data.meta_title,
+    description: settings.data.meta_description,
+  };
 };
 
 export default function RootLayout({
@@ -26,6 +34,7 @@ export default function RootLayout({
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
         <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
       </body>
+      <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
 }
